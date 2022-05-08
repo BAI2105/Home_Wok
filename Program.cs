f0340b8eea1a44511b3fcb1e275c9673ef246599
Александр <sania.bondarev2010@yandex.ru>
2022-05-08 17:03:11 +0300

    Add project Задача_1

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход
+//принимает два числа и выдает, какое 
+//число больше, а какое меньше.
+var n1 = Decimal.Parse(Console.ReadLine());
+         var n2 = Decimal.Parse(Console.ReadLine());
+if (n1 > n2)
+Console.WriteLine(n1);
+else
+Console.WriteLine(n2);
